0da26a669265c9e8bdcce36ad9b7f78b694aec9a
Maxim <maxrap7878@mail.ru>
2018-07-19 14:30:42 +0300

    add sections

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,30 +1,44 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import {Col, Grid,Row} from 'react-bootstrap';
+
 
 class App extends Component {
   render() {
     return (
-
+        <div className='main-container'>
           <div className="first" >
               <ul className='menu'>
-                  <li><a>lorem</a></li>
-                  <li><a>ipsum</a></li>
-                  <li><a>dolorm</a></li>
-                  <li><a>ametm</a></li>
-                  <li><a>conserm</a></li>
-                  <li><a>atadorm</a></li>
+                  <li><a>About me</a></li>
+                  <li><a>My skills</a></li>
+                  <li><a>My experience</a></li>
+                  <li><a>Motivation section</a></li>
               </ul>
               <div className="photo-container">
                   <img className='myphoto' alt='my photo' src={require('./images/iam.jpg')} />
-                  <div  className='photo-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Ab at dolor ducimus, facilis, illum incidunt iusto minima non,
-                      odio quae quam quia quisquam recusandae sint soluta tenetur vel!
-                      Distinctio, tempora.</div>
+                  <div className="photo-text-container">
+                  <p  className='photo-text'> Hello. My name is Max and I front-end developer </p>
+                      <p className="about-me">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                          Distinctio eveniet ipsa magnam molestias necessitatibus!
+                          A eligendi fugit laboriosam libero nulla possimus?
+                          Commodi consectetur esse hic itaque maiores nesciunt sequi vero?</p>
+                  </div>
               </div>
-
           </div>
+            <div className="second">
+                <img src={require('./images/react2.svg')} alt="react logo" className="react-logo"/>
+            </div>
+            <div className="third">
+
+            </div>
+            <div className="fouth">
+
+            </div>
+
+
+        </div>
+
+
 
     );
   }
